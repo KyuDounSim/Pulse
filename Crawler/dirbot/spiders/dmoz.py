8baec50b5b09700b6318a1d0c31d1fe767b39fa6
JaiGatsby <jais334@gmail.com>
2016-07-15 12:18:32 +0800

    Add more category options

--- Crawler/dirbot/spiders/dmoz.py
+++ Crawler/dirbot/spiders/dmoz.py
@@ -13,7 +13,9 @@ class DmozSpider(Spider):
     name = "dmoz"
     # allowed_domains = ["cse.ust.hk"]
     start_urls = [
-        "http://www.cse.ust.hk/News/",
+        "http://www.cse.ust.hk/News/?type=event",
+        "http://www.cse.ust.hk/News/?type=achievement",
+        "http://www.cse.ust.hk/News/?type=news",
 	"http://www.cse.ust.hk/pg/defenses/pastthesisdef.html",
         "http://www.cse.ust.hk/pg/seminars/pastseminars.html"
     ]
@@ -27,9 +29,26 @@ class DmozSpider(Spider):
     def parse_News(self,response):
 	# The following line selects all the urls in the maincontent dic, but not the ones in the hnav ul
 	# These are then appended to the list of urls to be parsed, each of which is parsed through parse_dir_contents
+        news = False
+        event = False
+        ach = False
+        if "news" in response.url:
+            news = True
+        elif "event" in response.url:
+            event = True
+        elif "achievement" in response.url:
+            ach = True
+        else:
+            print "Fuck"
 	for href in response.xpath('//div[@id="maincontent"]/ul[not(@class="hnav")]/li/a/@href'):
             url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_Contents)
+            if ach:
+                yield scrapy.Request(url, callback=self.parse_AContents)
+            elif event:
+                yield scrapy.Request(url, callback=self.parse_EContents)
+            else:
+                yield scrapy.Request(url, callback=self.parse_NContents)
+                
 ##        print "it worked"
 
     def parse_Other(self,response):
@@ -58,10 +77,74 @@ class DmozSpider(Spider):
         item['date'] = date[16:]
         yield item
     
-    def parse_Contents(self,response):
+    def parse_NContents(self,response):
         # This creates an instance of the item, the definition of which is stored in items.py
         item = Website()
         item['category'] = "News"
+        
+        # The if statement checks if there exists a div with id = 'maincontent', suggesting we are on the cse website, hence no error will be thrown
+        if (response.xpath('//div[@id="maincontent"]') != []):
+            for sel in response.xpath('//div[@id="maincontent"]'):
+                item['name'] = sel.xpath('h1/text()').extract()
+                temp = []
+                for ptag in sel.xpath('p'):
+                    temp += ptag.xpath('text()').extract()
+                        
+                
+        else:
+                item["name"] = response.xpath('//h1/text()').extract()
+                temp = []
+                for ptag in response.xpath('//p'):
+                        temp += ptag.xpath('text()').extract()
+        
+        description = "" 
+        for i in temp:
+                description += i
+        item['lenArticle']= description
+        # This line computes the sentiment of the current article, and adds it to the solution object
+##        item['vaderSentiment'] = vaderSentiment(item['description'])
+        # This basically gets the first part of the text from the right p tag of the footer, converts it into a str, and slices the needed part
+        dateText =  response.xpath('//div[@id="footer"]/p[@class="right"]/text()[1]').extract()
+        date = dateText[0]
+        item['date'] = date[16:]
+        yield item
+
+    def parse_EContents(self,response):
+        # This creates an instance of the item, the definition of which is stored in items.py
+        item = Website()
+        item['category'] = "Event"
+        
+        # The if statement checks if there exists a div with id = 'maincontent', suggesting we are on the cse website, hence no error will be thrown
+        if (response.xpath('//div[@id="maincontent"]') != []):
+            for sel in response.xpath('//div[@id="maincontent"]'):
+                item['name'] = sel.xpath('h1/text()').extract()
+                temp = []
+                for ptag in sel.xpath('p'):
+                    temp += ptag.xpath('text()').extract()
+                        
+                
+        else:
+                item["name"] = response.xpath('//h1/text()').extract()
+                temp = []
+                for ptag in response.xpath('//p'):
+                        temp += ptag.xpath('text()').extract()
+        
+        description = "" 
+        for i in temp:
+                description += i
+        item['lenArticle']= description
+        # This line computes the sentiment of the current article, and adds it to the solution object
+##        item['vaderSentiment'] = vaderSentiment(item['description'])
+        # This basically gets the first part of the text from the right p tag of the footer, converts it into a str, and slices the needed part
+        dateText =  response.xpath('//div[@id="footer"]/p[@class="right"]/text()[1]').extract()
+        date = dateText[0]
+        item['date'] = date[16:]
+        yield item
+        
+    def parse_AContents(self,response):
+        # This creates an instance of the item, the definition of which is stored in items.py
+        item = Website()
+        item['category'] = "Achievement"
         
         # The if statement checks if there exists a div with id = 'maincontent', suggesting we are on the cse website, hence no error will be thrown
         if (response.xpath('//div[@id="maincontent"]') != []):
