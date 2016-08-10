19c3030bfd167baa99ba28e773f8946feb6ca783
wangqianwen0418 <wangqianwen0418@163.com>
2016-08-10 09:39:02 +0800

    small changes

--- dynamic demo3/part2.js
+++ dynamic demo3/part2.js
@@ -65,12 +65,12 @@ var focus2 = svgRiver2
     .attr("class","focus2")
     .attr("transform","translate("+margin+","+(height2)+")");
 
-// var  stack = d3.stack().offset(d3.stackOffsetWiggle)
+var  stack2 = d3.stack().offset(d3.stackOffsetWiggle)
               
 
 // var areaF = d3.area()
 //     .curve(d3.curveBasis)
-//     .x(function(d) {return x(d.data.date); })
+//     .x(function(d) {return x(d.data2.date); })
 //     .y0(function(d) {return y(d[0]); })
 //     .y1(function(d) { return y(d[1]); });
 
@@ -98,25 +98,32 @@ var labelBox2=svgPie2.append("g")
 // var brush=d3.brushX()
 //           .extent([[0,0],[width,height2]]);
 
-// var stopFlag=1;
+ var stopFlag2=1;
 // var transitionSpeed=25;
 // var brushDomain
 // var finalDomain
+ var data2;
 
-d3.json("cseAll.json",function(error,data){
+$.ajax({
+  url: 'dataECE.json',
+  async: false,
+  dataType: 'json',
+  success: function (response) {
+    data2=response
+  }
+});
 
-if (error) throw error;
 
 
 var categoryList2=d3.nest()
                 .key(function(d){return d.category})
                 .sortKeys(d3.ascending)
-                .entries(data)
+                .entries(data2)
                 .map(function(d){
                   return d.key
                 })
 
-var colorCategory2=d3.scaleOrdinal()
+var colorCategory2=d3.scaleOrdinal(d3.schemeCategory10)
             .domain(categoryList2)
             .range(['#9bf2b4','#b153f2','#f27ca3','#f2e176','#5a92f2','#5fc5aa']);
 
@@ -136,65 +143,73 @@ var perMonth2=d3.nest()
            .rollup(function(leaves){
             return leaves.length
              })
-           .entries(data)
+           .entries(data2)
 
 
-var numberOfAchievement=d3.range(perMonth2.length).map(function(){return 0});
-var numberOfEvent=d3.range(perMonth2.length).map(function(){return 0});
-var numberOfNews=d3.range(perMonth2.length).map(function(){return 0});
-var numberOfThesisDefence=d3.range(perMonth2.length).map(function(){return 0});
-var numberOfSeminar=d3.range(perMonth2.length).map(function(){return 0})
+var numberOfStudentAchievement2=d3.range(perMonth2.length).map(function(){return 0});
+var numberOfEvent2=d3.range(perMonth2.length).map(function(){return 0});
+var numberOfSeminar2=d3.range(perMonth2.length).map(function(){return 0});
+var numberOfPhDThesisPresentation2=d3.range(perMonth2.length).map(function(){return 0});
+var numberOfMPhilThesisPresentation2=d3.range(perMonth2.length).map(function(){return 0});
+var numberOfFacultyAchievement2=d3.range(perMonth2.length).map(function(){return 0})
 
 var monthData2=perMonth2.map(function(d,i){
   for (var n = d.values.length - 1; n >= 0; n--) {
     switch(d.values[n].key)
     {
       case categoryList2[0]:
-      numberOfAchievement[i]+=d.values[n].value
+      numberOfEvent2[i]+=d.values[n].value
       break
 
       case categoryList2[1]:
-      numberOfEvent[i]+=d.values[n].value
+      numberOfFacultyAchievement2[i]+=d.values[n].value
       break
 
       case categoryList2[2]:
-      numberOfNews[i]+=d.values[n].value
+      numberOfMPhilThesisPresentation2[i]+=d.values[n].value
       break
 
       case categoryList2[3]:
-      numberOfSeminar[i]+=d.values[n].value
+      numberOfPhDThesisPresentation2[i]+=d.values[n].value
       break
 
       case categoryList2[4]:
-      numberOfThesisDefence[i]+=d.values[n].value
+      numberOfSeminar2[i]+=d.values[n].value
       break
+
+      case categoryList2[5]:
+      numberOfStudentAchievement2[i]+=d.values[n].value
+      break
+
+
 
     }
   }
   return{
     date:parseDate(d.key),
-    Achievement:numberOfAchievement[i],
-    Event:numberOfEvent[i],
-    News:numberOfNews[i],
-    ThesisDefense:numberOfThesisDefence[i],
-    Seminar:numberOfSeminar[i]
+    event:numberOfEvent2[i],
+    facultyAchievement:numberOfFacultyAchievement2[i],
+    MPhilThesisPresentation:numberOfMPhilThesisPresentation2[i],
+    PhDThesisPresentation:numberOfPhDThesisPresentation2[i],
+    seminar:numberOfSeminar2[i],
+    studentAchievement:numberOfStudentAchievement2[i]
 
   }
 })
 
-var wholePeriod=moment(monthData2[monthData2.length-1].date,"YYYY-MM").year()*12+moment(monthData2[monthData2.length-1].date,"YYYY-MM").month()-moment(monthData2[0].date,"YYYY-MM").year()*12-moment(monthData2[0].date,"YYYY-MM").month()
+var wholePeriod2=moment(monthData2[monthData2.length-1].date,"YYYY-MM").year()*12+moment(monthData2[monthData2.length-1].date,"YYYY-MM").month()-moment(monthData2[0].date,"YYYY-MM").year()*12-moment(monthData2[0].date,"YYYY-MM").month()
 
-// var monthDataNoGap=d3.range(wholePeriod).map(function(){return 0})
+// var monthData2NoGap=d3.range(wholePeriod2).map(function(){return 0})
 // var n=0
-// for (var i = 0; i<wholePeriod;i--) 
-// {  var time=moment(monthData2[0].date,"YYYY-MM").add(i,"month").format("YYYY-MM")
+// for (var i = 0; i<wholePeriod2;i--) 
+// {  var time=moment(monthData22[0].date,"YYYY-MM").add(i,"month").format("YYYY-MM")
     
-//     if (time==monthData2[n].date)
-//         {monthDataNoGap[i]=monthData2[n];
+//     if (time==monthData22[n].date)
+//         {monthData2NoGap[i]=monthData22[n];
 //           n+=1;
 //         }
 //      else
-//      monthDataNoGap[i]=({
+//      monthData2NoGap[i]=({
 //         date:time,
 //         Achievement:0,
 //         Event:0,
@@ -208,21 +223,22 @@ var wholePeriod=moment(monthData2[monthData2.length-1].date,"YYYY-MM").year()*12
 // //
 // //
 // //
-// // console.log(monthDataNoGap)
+// // console.log(monthData2NoGap)
 
-var riverData=d3.range(12).map(function(d,i){
+var riverData2=d3.range(12).map(function(d,i){
 return monthData2[i]
 })
 
-stack.keys(categoryList2)
+stack2.keys(categoryList2)
+// console.log(categoryList2)
 
-var layers = stack(riverData);
-var layersY=stack(monthData2)
+var layers2 = stack2(riverData2);
+var layers2Y=stack2(monthData2)
 
-// x.domain(d3.extent(layers[0].map(function(d) {return d.data.date })))
+// x.domain(d3.extent(layers2[0].map(function(d) {return d.data2.date })))
 
-// y.domain([d3.min(layersY, function(layer) { return d3.min(layer, function(d) { return Math.min(d[0],d[1]); }); }), 
-//   d3.max(layersY, function(layer) { return d3.max(layer, function(d) { return Math.max(d[0],d[1]); }); })*1.3])
+// y.domain([d3.min(layers2Y, function(layer) { return d3.min(layer, function(d) { return Math.min(d[0],d[1]); }); }), 
+//   d3.max(layers2Y, function(layer) { return d3.max(layer, function(d) { return Math.max(d[0],d[1]); }); })*1.3])
 
 // yAxis.ticks(y.domain()[1]-y.domain()[0]+1)
 
@@ -230,11 +246,11 @@ focus2.append("g")
   .attr("transform","translate(5,30)")
   .attr("class","background")
   .append("text")
-  .text("CSE")
+  .text("ECE")
   .attr("opacity",0.3)
 
 focus2.selectAll("path")
-  .data(layers)
+  .data(layers2)
   .enter()
   .append("g")
   .append("path")
@@ -268,29 +284,29 @@ focus2.append("g")
       .attr("transform", "translate(0,"+(height+5)+" )")
       .call(xAxis);
 
-var filter=[]
+var filter2=[]
   
-  for (var i =data.length - 1; i >= 0; i--) {
+  for (var i =data2.length - 1; i >= 0; i--) {
 
-  if(parseDate(moment(data[i].date,"YYYY-MM-DD").format("YYYY-MM"))>x.domain()[0]&&
-    parseDate(moment(data[i].date,"YYYY-MM-DD").format("YYYY-MM"))<x.domain()[1])
-    filter.push(data[i]);
+  if(parseDate(moment(data2[i].date,"YYYY-MM-DD").format("YYYY-MM"))>x.domain()[0]&&
+    parseDate(moment(data2[i].date,"YYYY-MM-DD").format("YYYY-MM"))<x.domain()[1])
+    filter2.push(data2[i]);
     }
  
 
-  var reOrder=d3.nest()
+  var reOrder2=d3.nest()
                     .key(function(d){return d.category})
                     .rollup(function(d){return d.length})
-                    .entries(filter)
+                    .entries(filter2)
 
 r.domain([20,200])
-arc.outerRadius(r(filter.length));
+arc.outerRadius(r(filter2.length));
 
-var orderByCategory=categoryList2.map(function(d,i){
+var orderByCategory2=categoryList2.map(function(d,i){
     var n=0;
-    for (var i = reOrder.length - 1; i >= 0; i--) {
-      if(reOrder[i].key==d)
-        {n+=reOrder[i].value;}
+    for (var i = reOrder2.length - 1; i >= 0; i--) {
+      if(reOrder2[i].key==d)
+        {n+=reOrder2[i].value;}
     }
     return{
         key:d,
@@ -298,8 +314,8 @@ var orderByCategory=categoryList2.map(function(d,i){
       }
   })
 
-var piePath=pieGraph2.selectAll(".arc")
-  .data(pie(orderByCategory))
+var piePath2=pieGraph2.selectAll(".arc")
+  .data(pie(orderByCategory2))
   .enter()
   .append("g")
   .append("path")
@@ -324,12 +340,12 @@ var piePath=pieGraph2.selectAll(".arc")
       labelBox2.selectAll("."+selectedCategory).style("font-Size",17);
     }) 
 
-  d3.select("#startTime").html(moment(x.domain()[0]).format("YYYY-MMM"));
-    d3.select("#endTime").html(moment(x.domain()[1]).format("YYYY-MMM"));
-    d3.select("#totalNumber").html("Total Number:"+filter.length);
+  // d3.select("#startTime").html(moment(x.domain()[0]).format("YYYY-MMM"));
+  //   d3.select("#endTime").html(moment(x.domain()[1]).format("YYYY-MMM"));
+    d3.select("#totalNumber2").html("Total Number:"+filter2.length);
 
-var labelBoxes=labelBox2.selectAll("#labelBox2")
-        .data(orderByCategory)
+var labelBoxes2=labelBox2.selectAll("#labelBox2")
+        .data(orderByCategory2)
         .enter()
         .append("g")
         .attr("opacity",0.6)
@@ -353,7 +369,7 @@ var labelBoxes=labelBox2.selectAll("#labelBox2")
 ;
 
 
-var labelRect = labelBoxes.append("rect")
+var labelRect2 = labelBoxes2.append("rect")
         .attr("rx",4)
         .attr("ry",4)
         .attr("x",20+margin)
@@ -368,7 +384,7 @@ var labelRect = labelBoxes.append("rect")
         })
         
 
-var labelCategory = labelBoxes.append("text")
+var labelCategory2 = labelBoxes2.append("text")
         .text(function(d){
             return d.key;
         })
@@ -378,8 +394,8 @@ var labelCategory = labelBoxes.append("text")
             return margin+height2+35+index*30;
         })
 
-var labelNumber=labelBoxes.append("text")
-        .attr("class","labelNumber")
+var labelNumber2=labelBoxes2.append("text")
+        .attr("class","labelNumber2")
         .text(function(d){
             return d.value;
         })
@@ -389,32 +405,50 @@ var labelNumber=labelBoxes.append("text")
             return margin+height2+35+index*30;
         })
 
-var totalNumber=labelBoxes.append("text")
-        .attr("id","totalNumber")
+var totalNumber2=labelBoxes2.append("text")
+        .attr("id","totalNumber2")
         .attr("x",30+margin)
         .attr("y",margin+height2+35+(categoryList2.length)*30)
 
 var n=0
-var handle=0
-handle=setInterval(function(){
-  if(n<wholePeriod-12){
-  transition();
- n++}
-  else
-  kill()},duration*1.1)
-
-function kill(){
-  clearInterval();
-  handle=0;
+var handle2=0
+if(n<wholePeriod2-12){
+handle2=setInterval(function(){
+  transition2();
+    n++
+    },duration*1.1)
 }
 
-function transition() {
-riverData=d3.range(12).map(function(d,i){
+function kill2(){
+  if(stopFlag2==1)
+  {
+  clearInterval(handle2);
+  handle2=0;
+  stopFlag2=0;
+  }
+  else if(n<wholePeriod2-12)
+  {
+   handle2=setInterval(function(){
+
+    transition2();
+    n++}
+    ,duration*1.1)
+
+   
+  }
+}
+
+function stop2(){
+  stopFlag2=2
+  clearInterval(handle2);
+}
+function transition2() {
+riverData2=d3.range(12).map(function(d,i){
 return monthData2[i+n]})
 
-layers = stack(riverData);
+layers2 = stack2(riverData2);
 
-x.domain(d3.extent(layers[0].map(function(d) {return d.data.date })))
+x.domain(d3.extent(layers2[0].map(function(d) {return d.data.date })))
 
 focus2.selectAll('.xAxis').remove()
 
@@ -425,32 +459,32 @@ focus2.append("g")
 
 
 focus2.selectAll("path")
-  .data(layers)
+  .data(layers2)
   .transition()
   .duration(duration)
   .ease(d3.easeLinear)
   .attr("d",areaF) 
 
-filter=[]
+filter2=[]
   
-  for (var i =data.length - 1; i >= 0; i--) {
+  for (var i =data2.length - 1; i >= 0; i--) {
 
-  if(parseDate(moment(data[i].date,"YYYY-MM-DD").format("YYYY-MM"))>x.domain()[0]&&
-    parseDate(moment(data[i].date,"YYYY-MM-DD").format("YYYY-MM"))<x.domain()[1])
-    filter.push(data[i]);
+  if(parseDate(moment(data2[i].date,"YYYY-MM-DD").format("YYYY-MM"))>x.domain()[0]&&
+    parseDate(moment(data2[i].date,"YYYY-MM-DD").format("YYYY-MM"))<x.domain()[1])
+    filter2.push(data2[i]);
     }
  
 
-  var reOrder=d3.nest()
+  var reOrder2=d3.nest()
                     .key(function(d){return d.category})
                     .rollup(function(d){return d.length})
-                    .entries(filter)
+                    .entries(filter2)
 
-orderByCategory=categoryList2.map(function(d,i){
+orderByCategory2=categoryList2.map(function(d,i){
     var n=0;
-    for (var i = reOrder.length - 1; i >= 0; i--) {
-      if(reOrder[i].key==d)
-        {n+=reOrder[i].value;}
+    for (var i = reOrder2.length - 1; i >= 0; i--) {
+      if(reOrder2[i].key==d)
+        {n+=reOrder2[i].value;}
     }
     return{
         key:d,
@@ -458,17 +492,17 @@ orderByCategory=categoryList2.map(function(d,i){
       }
   })
 
-// arc.outerRadius(r(filter.length))
+// arc.outerRadius(r(filter2.length))
 
 // pieGraph2.selectAll("path")
-//   .data(pie(orderByCategory))
+//   .data2(pie(orderByCategory2))
 //   .transition()
 //   .duration(duration)
 //   .ease(d3.easeLinear)
 //   .attr("d",arc) 
 
-piePath
-.data(pie(orderByCategory))
+piePath2
+.data(pie(orderByCategory2))
 .transition()
 .duration(duration)
 .ease(d3.easeLinear)
@@ -477,24 +511,24 @@ piePath
    var i = d3.interpolate(this._current, a);
     this._current=i(0)
 
-    var k=d3.interpolate(arc.outerRadius()(),r(filter.length))
+    var k=d3.interpolate(arc.outerRadius()(),r(filter2.length))
     return function(t) {
     return arc.outerRadius(k(t))(i(t));
   };
   
  })
 
-d3.select("#startTime").html(moment(x.domain()[0]).format("YYYY-MMM"));
-    d3.select("#endTime").html(moment(x.domain()[1]).format("YYYY-MMM"));
-    d3.select("#totalNumber").html("Total Number:"+filter.length);
+// d3.select("#startTime").html(moment(x.domain()[0]).format("YYYY-MMM"));
+//     d3.select("#endTime").html(moment(x.domain()[1]).format("YYYY-MMM"));
+     d3.select("#totalNumber2").html("Total Number:"+filter2.length);
 
-labelBoxes.selectAll(".labelNumber").remove()
+labelBoxes2.selectAll(".labelNumber2").remove()
 
-    labelBoxes=labelBox2.selectAll("#labelBox2")
-        .data(orderByCategory)
+    labelBoxes2=labelBox2.selectAll("#labelBox2")
+        .data(orderByCategory2)
 
-    labelNumber=labelBoxes.append("text")
-        .attr("class","labelNumber")
+    labelNumber2=labelBoxes2.append("text")
+        .attr("class","labelNumber2")
         .text(function(d){
               return d.value;
         })
@@ -508,5 +542,5 @@ labelBoxes.selectAll(".labelNumber").remove()
 
 
 
-});
+
 
